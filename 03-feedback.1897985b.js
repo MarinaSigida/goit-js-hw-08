d1bdf3d9a6e5176175cc42345b2994deebc09af0
MarinaSigida <122404653+MarinaSigida@users.noreply.github.com>
2023-05-28 21:58:16 +0000

    Deploying to gh-pages from @ MarinaSigida/goit-js-hw-08@c1fc770ce445ccbf77dd29f3d85f2fed2d37a58c 🚀

---
+++ 03-feedback.1897985b.js
@@ -0,0 +1,2 @@
+[document.querySelector("form"),document.querySelector(".feedback-form input"),document.querySelector(".feedback-form textarea")][0].addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}));
+//# sourceMappingURL=03-feedback.1897985b.js.map
---
+++ 03-feedback.6f21350a.js
@@ -0,0 +1,2 @@
+({form:document.querySelector("form"),email:document.querySelector(".feedback-form input"),message:document.querySelector(".feedback-form textarea")}).form.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}));
+//# sourceMappingURL=03-feedback.6f21350a.js.map
