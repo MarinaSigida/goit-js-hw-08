28f4a8b11bb1a8a6894aa7dc21fd5a87efe60d21
MarinaSigida <sigida.web@gmail.com>
2023-05-28 17:31:26 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems.map(({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+    <a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        alt=${description}
+      />
+    </a>
+  </li>
+  `;
+});
+
+console.log(galleryItem);
+
+gallery.insertAdjacentHTML("beforeend", galleryItem.join(""));
+
+
+
+  // Modal window with simple ligthbox
+  const lightbox = new SimpleLightbox(".gallery a", {
+    // captionType: text,
+    captionsData: 'alt',
+    captionPosition:'bottom',
+    captionsDelay: 250,
+  });
+
