c1fc770ce445ccbf77dd29f3d85f2fed2d37a58c
MarinaSigida <sigida.web@gmail.com>
2023-05-28 23:57:52 +0200

    update  1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import trottle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const inframe = document.querySelector('iframe');
+const player = new Player(inframe);
+const KeyStorage = 'videoplayer-current-time';
+const time = JSON.parse(localStorage.getItem(KeyStorage));
+
+// Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player,
+// але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+// Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+// Зберігай час відтворення у локальне сховище.
+// Нехай ключем для сховища буде рядок "videoplayer-current-time".
+
+player.on(
+  'timeupdate',
+  trottle(function (data) {
+    localStorage.setItem(KeyStorage, JSON.stringify(data.seconds));
+  }),
+  1000
+);
+
+// Під час перезавантаження сторінки скористайся методом setCurrentTime()
+// з метою відновлення відтворення зі збереженої позиції.
+
+
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    seconds=JSON.parse(localStorage.getItem(KeyStorage))
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+const KEY_STORAGE = "feedback-form-state";
+const refs = {
+    form: document.querySelector('form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea')
+};
 
+refs.form.addEventListener('input', onFormInput);
+// refs.email.addEventListener('input', onEmailInput);
+// refs.message.addEventListener('input', onTextAreaInput);
+
+function onFormInput (event) {
+    const text = event.currentTarget.value;
+    localStorage.setItem(KEY_STORAGE, text)
+}
